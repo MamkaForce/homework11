1b5e0c9a3b89c0c95799458b8e394c7dcc5545c6
MamkaForce <47280466+MamkaForce@users.noreply.github.com>
2019-02-28 20:02:27 +0200

    homework11

---
+++ Walkthrough.js
@@ -0,0 +1,104 @@
+1;  var i = 0;
+    while (i < 2) {
+        north(i);
+        i++;
+}
+
+2;  var i = 0;
+    while (i < 16) {
+        east(i);
+        i++;
+} 
+
+3;  var i = 0;
+    while (i < 2) {
+        south(i);
+        i++;
+
+    while (i < 2){
+        east(i);
+        break;
+    }
+}
+//7 раз
+ 
+
+	
+4; var i = 0;
+    while (i < 4){
+        east(i);
+        i++;
+    }
+    
+    var i = 0;
+    while (i < 3){
+        south(i);
+        i++;
+    }
+ 
+    var i = 0;
+    while (i < 2){
+        east(i);
+        i++;
+    }
+    
+    var i = 0;
+    while (i <  3){
+        south(i);
+        i++;
+    }
+
+    var i = 0;
+    while (i < 4){
+        east(i);
+        i++;
+    }
+    
+    var i = 0;
+    while (i < 2){
+        south(i);
+        i++;
+    }
+
+5;  var i = 0;
+    while (i < 5){
+        east(i);
+        i++;
+    }
+    
+    var i = 0;
+    while (i < 4){
+        south(i);
+        i++;
+    }
+    
+    var i = 0;
+    while (i < 3){
+        west(i);
+        i++;
+    }
+    
+    var i = 0;    
+    while (i < 4){
+        south(i);
+        i++;
+    }
+
+    var i = 0;
+    while (i < 3){
+        west(i);
+        i++;
+    }
+
+    var i = 0;    
+    while (i < 4);{
+        north(i);
+        i++;
+    }   
+    
+    var i = 0;    
+    while (i < 3){
+            south(i);
+            i++;
+        }
+    
